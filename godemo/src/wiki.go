048a3c8aeba10344218b5254d524cb6160160305
Allan <allanxy@gmail.com>
2013-05-03 00:48:21 +0800

    提交修改

--- godemo/src/wiki.go
+++ godemo/src/wiki.go
@@ -3,7 +3,7 @@ package main
 import (
 	"io/ioutil"
 	"net/http"
-	"os"
+	//"os"
 	"regexp"
 	"text/template"
 )
@@ -13,12 +13,12 @@ type Page struct {
 	Body  []byte
 }
 
-func (p Page) save() os.Error {
+func (p Page) save() error {
 	filename := p.Title + ".txt"
 	return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
-func loadPage(title string) (*Page, os.Error) {
+func loadPage(title string) (*Page, error) {
 	filename := title + ".txt"
 	body, err := ioutil.ReadFile(filename)
 	if err != nil {
@@ -49,7 +49,8 @@ func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p := &Page{Title: title, Body: []byte(body)}
 	err := p.save()
 	if err != nil {
-		http.Error(w, err.String(), http.StatusInternalServerError)
+	
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
@@ -59,7 +60,7 @@ var templates = make(map[string]*template.Template)
 
 func init() {
 	for _, tmpl := range []string{"edit", "view"} {
-		t := template.Must(template.ParseFile("template/" + tmpl + ".html"))
+		t := template.Must(template.New(tmpl).ParseFiles("template/" + tmpl + ".html"))
 		templates[tmpl] = t
 	}
 }
@@ -67,7 +68,7 @@ func init() {
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	err := templates[tmpl].Execute(w, p)
 	if err != nil {
-		http.Error(w, err.String(), http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
@@ -77,7 +78,7 @@ var titleValidator = regexp.MustCompile("^[a-zA-Z0-9]+$")
 
 func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		title := r.URL.Path[lenPath:]
+		title := r.URL.Path[lenlenPath:]
 		if !titleValidator.MatchString(title) {
 			http.NotFound(w, r)
 			return
